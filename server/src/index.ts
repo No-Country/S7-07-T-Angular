c826a5459a8b30379856e9e0e12360b08478504a
Miguel Guerrero <yannokaiserfrom@gmail.com>
2023-03-27 23:52:35 -0300

    config db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv"
-dotenv.config()
+import db from "./db/config"
+dotenv.config({ path: '../.env' });
+db
+
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
