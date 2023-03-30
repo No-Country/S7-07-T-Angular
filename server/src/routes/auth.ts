5344b1dca9ed0d6a1f5e9be446754dbaef5c70aa
Juan Mosquella <jmosquella11@gmail.com>
2023-03-30 15:29:03 -0300

    OTP verification added, reset password

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,5 +1,13 @@
 import { Router } from "express";
-import { register, login } from "../controller/auth";
+import {
+  register,
+  login,
+  generateOTP,
+  verifyOTP,
+  createResetSession,
+  resetPassword,
+} from "../controller/auth";
+import localVariables from "../middlewares/local-variables";
 import { rulesLogin } from "../validators/rulesLogin";
 import { rulesRegister } from "../validators/rulesRegister";
 import { validationGeneral } from "../validators/validationGeneral";
@@ -8,4 +16,9 @@ const route = Router();
 
 route.post("/register", rulesRegister, validationGeneral, register);
 route.post("/login", rulesLogin, validationGeneral, login);
+route.get("/generateOTP", localVariables, generateOTP);
+route.get("/verifyOTP", verifyOTP);
+route.get("/create-reset-session", createResetSession);
+route.put("/reset-password", resetPassword);
+
 export default route;
