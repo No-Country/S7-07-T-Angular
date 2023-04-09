2c7f10ce54bc9133fe541dcb1ec22b9c98e3f444
nicolle <niicostars04@gmail.com>
2023-04-09 19:46:45 -0400

    agregando contenido de componentes

--- Angular/proyect/src/app/app.module.ts
+++ Angular/proyect/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/navbar/shared.module';
 import { FooterComponent } from './shared/footer/footer.component';
+import { RoutinesComponent } from './lading-page/routines/routines.component';
+import { BenefitsComponent } from './lading-page/benefits/benefits.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RoutinesComponent,
+    BenefitsComponent,
     FooterComponent
   ],
   imports: [
